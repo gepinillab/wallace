30f25ccf5b09fd590221633cbbaecc4248a2f071
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2020-08-24 11:45:40 -0500

    create modules with create_module()

---
+++ inst/shiny/modules/demo_function2.R
@@ -0,0 +1,55 @@
+demo_function2_module_ui <- function(id) {
+  ns <- shiny::NS(id)
+  tagList(
+    # UI
+    actionButton(ns("run"), "Run module demo_function2")
+  )
+}
+
+demo_function2_module_server <- function(input, output, session, common) {
+
+  observeEvent(input$run, {
+    # WARNING ####
+
+    # FUNCTION CALL ####
+
+    # LOAD INTO SPP ####
+
+    # METADATA ####
+  })
+
+  output$result <- renderText({
+    # Result
+  })
+
+  return(list(
+    save = function() {
+      # Save any values that should be saved when the current session is saved
+    },
+    load = function(state) {
+      # Load
+    }
+  ))
+
+}
+
+demo_function2_module_result <- function(id) {
+  ns <- NS(id)
+
+  # Result UI
+  verbatimTextOutput(ns("result"))
+}
+
+demo_function2_module_map <- function(map, common) {
+  # Map logic
+}
+
+demo_function2_module_rmd <- function(species) {
+  # Variables used in the module's Rmd code
+  list(
+    demo_function2_knit = species$rmm$code$wallace$someFlag,
+    var1 = species$rmm$code$wallace$someSetting1,
+    var2 = species$rmm$code$wallace$someSetting2
+  )
+}
+
